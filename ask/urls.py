1256b067301905c53c1660736669fdd01a739983
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-02-01 20:17:23 +0600

    Question detail view created

--- ask/urls.py
+++ ask/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('', views.home, name='ask-home'),
     path('', views.ques_list, name='ques_list'),
     path('about/', views.about, name='ask-about'),
+    path('question/<int:pk>/', views.ques_detail, name = 'ques_detail'),
 
 
 ]
--- ask/views.py
+++ ask/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -16,3 +16,7 @@ def about(request):
 def ques_list(request):
     ques = Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'ask/ques_list.html', {'ques': ques})
+
+def ques_detail(request, pk):
+    que = get_object_or_404(Question, pk=pk)
+    return render(request, 'ask/ques_detail.html', {'que': que})
