ec3522c0bba1a7d35a3816701ea7d79a4a42b38e
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-02-02 07:14:36 +0600

    Update urls.py

--- ask/urls.py
+++ ask/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('', views.ques_list, name='ques_list'),
     path('about/', views.about, name='ask-about'),
     path('question/<int:pk>/', views.ques_detail, name = 'ques_detail'),
+    path('question/new/', views.ques_new, name = 'ques_new'),
+    path('question/<int:pk>/edit/', views.ques_edit, name = 'ques_edit'),
+    path('drafts/', views.ques_draft_list, name='ques_draft_list'),
+    path('question/<pk>/publish/', views.ques_publish, name='ques_publish'),
 
 
 ]
