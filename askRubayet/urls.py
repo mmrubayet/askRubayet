7980e80443d251fa9f0f4a125a87695413f65c49
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-02-02 20:59:46 +0600

    update

--- askRubayet/urls.py
+++ askRubayet/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
-from ask import views as askViews
+# from ask import views as askViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
