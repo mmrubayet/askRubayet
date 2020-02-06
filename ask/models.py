cc2613a6b1f3cc5cde5bd16d1d13b1de0ae3124b
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-02-07 00:44:07 +0600

    answer model created

--- ask/models.py
+++ ask/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     author                  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title                   = models.CharField(max_length=300)
     optional_description    = models.TextField(blank=True, null=True)
-    answer                  = models.TextField(blank=True, null=True)
+    # answer                  = models.TextField(blank=True, null=True)
     created_date            = models.DateTimeField(default=timezone.now)
     published_date          = models.DateTimeField(blank=True, null=True)
 
@@ -22,3 +22,21 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Answer(models.Model):
+    question            = models.ForeignKey('ask.question', on_delete=models.CASCADE, related_name='answer')
+    # author              = models.CharField(max_length=200)
+    author              = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text                = models.TextField()
+    created_date        = models.DateTimeField(default=timezone.now)
+    approved_answer     = models.BooleanField(default=False)
+
+
+    def approve(self):
+        self.approved_answer = True
+        self.save()
+
+    def __str__(self):
+        return self.text
